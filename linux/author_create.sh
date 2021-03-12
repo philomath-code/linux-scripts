22264e0816bb52b4d6e3226057110205d1bfb150
Daniel Medina <philomathbooks@gmail.com>
2021-03-12 17:04:29 -0600

    angular scripts

---
+++ linux/author_create.sh
@@ -0,0 +1,10 @@
+Author="Philomath Code"
+WrittenBy="Author: $Author"
+GithubContact="Github: https://github.com/philomath-code"
+echo
+Dashes="<|>---------------------------------------<|>"
+echo $Dashes
+echo $WrittenBy
+echo $GithubContact
+echo $Dashes
+echo
---
+++ linux/create_new_angular_app_input.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+CurrentCLI=$(ng --version | grep -i "angular cli:")
+app="-app"
+dashes=$(echo "------------------")
+InitialDirectory=$(readlink -f .)
+DateDirectory=$(date +%m%d_%X)
+
+Author=" Philomath Code"
+WrittenBy=" Author: $Author"
+GithubContact="Github: https://github.com/philomath-code"
+echo
+Dashes="<|>---------------------------------------<|>"
+echo $Dashes
+echo $WrittenBy
+echo $GithubContact
+echo $Dashes
+echo
+echo "create a new angular application"
+echo $dashes
+echo
+echo "please enter application name"
+read input1
+
+userinput="$input1$app"
+GenesisDirectory="$userinput$DateDirectory"
+
+cd ~/Desktop
+mkdir $GenesisDirectory
+cd $GenesisDirectory
+GenesisDirectoryAbsolutePath=$(readlink -f .)
+echo "creating project at: $GenesisDirectoryAbsolutePath"
+
+ng new --skip-git=true --routing=true --style=scss $userinput
+location=$(readlink -f $userinput)
+cd $InitialDirectory
+echo "app location: $location"
+echo "done installing: $userinput !"
+echo $dashes
