5c8a889ceb9cecb5ccfb5c5c3cb0e245cdbf61d4
philomath <philomathbooks@gmail.com>
2021-11-03 12:08:51 -0500

    latest

--- linux/angular/author_create.sh
+++ linux/angular/author_create.sh
@@ -1,10 +1,8 @@
 Author="Philomath Code"
 WrittenBy="Author: $Author"
-GithubContact="Github: https://github.com/philomath-code"
 echo
 Dashes="<|>---------------------------------------<|>"
 echo $Dashes
 echo $WrittenBy
-echo $GithubContact
 echo $Dashes
 echo
--- linux/angular/create_new_angular_app_input.sh
+++ linux/angular/create_new_angular_app_input.sh
@@ -9,12 +9,12 @@ DateDirectory=$(date +%m%d_%X)
 
 Author=" Philomath Code"
 WrittenBy=" Author: $Author"
-GithubContact="Github: https://github.com/philomath-code"
+
 echo
 Dashes="<|>---------------------------------------<|>"
 echo $Dashes
 echo $WrittenBy
-echo $GithubContact
+
 echo $Dashes
 echo
 echo "create a new angular application"
